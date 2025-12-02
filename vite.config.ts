79556d7d7e09f43f5472cb53869ea5cb94c88358
OmeirMustafa <kaziomeirmustafa@gmail.com>
2025-12-02 20:19:12 +0600

    Force fix PostCSS config and lockfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js', // Explicitly point to the config
+  },
   server: {
     port: 3000
   }
